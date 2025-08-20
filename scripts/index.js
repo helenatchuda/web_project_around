ee95c380698e106501e831dbf6747ca825b91ae2
Helena <helenatchuda@gmail.com>
2025-08-20 22:02:27 +0100

    add new ards and bottom of submit informations

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-const profileInfo = document.querySelector(`.profile__container`);
+const cardTemplate = document.querySelector("#card-template").content;
 const elementContainer = document.querySelector(".cards");
+const profileInfo = document.querySelector(".profile__container");
 
 const nameElement = profileInfo.querySelector(".profile__name");
 const descriptionElement = profileInfo.querySelector(".profile__description");
@@ -8,8 +9,38 @@ const imagePopup = document.querySelector("#image-popup");
 const popupImage = imagePopup.querySelector(".popup__image");
 const titleImage = imagePopup.querySelector(".popup__image-title");
 const iditbutton = document.querySelector(".profile__edit-button");
-const formElement = document.querySelector(".popup__form-edit-name");
+const formElement = document.querySelector(".popup__form");
+
 const xclose = document.querySelector(".popup__close");
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+
+
 imagePopup.addEventListener("click", (event) => {
   const clickedOutside = !event.target.closest(".popup__container-image");
   if (clickedOutside) {
@@ -52,7 +83,8 @@ profileInfo.addEventListener("click", (event) => {
 
 function openPopup(type) {
   const popupElement = document.querySelector(`.popup[data-type="${type}"]`);
-
+ console.log("popup-element",popupElement);
+ console.log(`.popup[data-type="${type}"]`)
   if (type === "edit") {
     const inputName = popupElement.querySelector("#name");
     const inputDescription = popupElement.querySelector("#description");
@@ -77,11 +109,12 @@ function closePopup(popupElement) {
 
 function submitForm(event) {
   event.preventDefault();
+  console.log("submitfirm")
 
   const form = event.target;
   const popupElement = form.closest(".popup");
   const type = popupElement.dataset.type;
-
+ console.log(type)
   if (type === "edit") {
     const inputName = popupElement.querySelector("#name");
     const inputDescription = popupElement.querySelector("#description");
@@ -90,8 +123,9 @@ function submitForm(event) {
     descriptionElement.textContent = inputDescription.value;
     popupElement.classList.remove("popup__opened");
   }
-
+   console.log(type)
   if (type === "new-card") {
+    console.log("este é formulario do card");
     const title = popupElement.querySelector("#titulo").value;
     const link = popupElement.querySelector("#url-link").value;
 
@@ -105,11 +139,9 @@ function submitForm(event) {
 
   closePopup(popupElement);
 }
-if (formElement) {
-  formElement.addEventListener("submit", (e) => submitForm(e));
-} else {
-  console.error("Formulário não encontrado no DOM!");
-}
+
+formElement.addEventListener("submit", (e) => submitForm(e));
+
 
 if (xclose) {
   xclose.addEventListener("click", () =>
@@ -118,3 +150,31 @@ if (xclose) {
 } else {
   console.warn("Elemento '.popup__close' não encontrado.");
 }
+
+function addCard(card) {
+
+  const cardElement = cardTemplate.cloneNode(true);
+
+
+  const img = cardElement.querySelector(".card__image");
+  const titleElement = cardElement.querySelector(".card__title");
+
+  img.src = card.link;
+  img.alt = card.name;
+  titleElement.textContent = card.name;
+
+
+ return cardElement
+
+
+}
+function renderCard(data, wrap){
+  wrap.prepend(addCard(data))
+}
+
+initialCards.forEach((card)=>{
+  renderCard(card,elementContainer)
+})
+
+
+
