b858a6d487736792686c1033bbee9911c5b99e18
Helena <helenatchuda@gmail.com>
2025-10-21 21:58:18 +0100

    retruturaçao do codigo

---
+++ scripts/components/PopupWithForm.js
@@ -0,0 +1,34 @@
+// scripts/PopupWithForm.js
+import { Popup } from "./Popup.js";
+
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector("form");
+    this._inputList = Array.from(this._form.querySelectorAll("input"));
+  }
+
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach(input => {
+      formValues[input.name || input.id] = input.value;
+    });
+    return formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      const inputValues = this._getInputValues();
+      this._handleFormSubmit(inputValues);
+      this.close();  // fechar após submissão
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
---
+++ scripts/components/UserInfo.js
@@ -0,0 +1,23 @@
+// scripts/UserInfo.js
+export class UserInfo {
+  constructor({ nameSelector, descriptionSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._descriptionElement = document.querySelector(descriptionSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      description: this._descriptionElement.textContent
+    };
+  }
+
+  setUserInfo({ name, description }) {
+    if (name !== undefined) {
+      this._nameElement.textContent = name;
+    }
+    if (description !== undefined) {
+      this._descriptionElement.textContent = description;
+    }
+  }
+}
